409e1de1106379d5f39cdfb51f6de83af10281f7
mananpal1997 <brunodicosta@gmail.com>
2016-06-26 15:17:01 +0530

    positive lookbehind

---
+++ positive-lookbehind.php
@@ -0,0 +1,10 @@
+<?php
+$Regex_Pattern = '/(?<=[13579])[0-9]/'; //Do not delete '/'. Replace __________ with your regex. 
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+
+preg_match_all($Regex_Pattern, $Test_String, $output_array);
+printf("Number of matches : %d",count($output_array[0]));
+fclose($handle);
+?>
