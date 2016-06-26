b4bb4d2246dd5af371bbdb6eeae0dba40c81d972
mananpal1997 <brunodicosta@gmail.com>
2016-06-26 15:16:38 +0530

    negative lookbehind

---
+++ negative-lookbehind.php
@@ -0,0 +1,10 @@
+<?php
+$Regex_Pattern = '/(?<![aeiouAEIOU])[\s\S]/'; //Do not delete '/'. Replace __________ with your regex. 
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+
+preg_match_all($Regex_Pattern, $Test_String, $output_array);
+printf("Number of matches : %d",count($output_array[0]));
+fclose($handle);
+?>
