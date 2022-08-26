c08f3cc8c67119e1671c276b82403e7e736ae439
Abbas Majidov <69363088+absmj@users.noreply.github.com>
2022-08-26 10:39:01 +0400

    Create matching-same-text-again-again.php

---
+++ matching-same-text-again-again.php
@@ -0,0 +1,5 @@
+<?php
+
+$Regex_Pattern = '/^([a-z])([\w])([\s])([\W])([\d])([\D])([A-Z])([a-zA-Z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10$/'; //Do not delete '/'. Replace __________ with your regex.
+
+$handle = fopen ("php://stdin","r");
